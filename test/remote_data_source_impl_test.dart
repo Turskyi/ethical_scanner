8f3dccc8b2a5a9937e05e934b8db30facb424ef2
Dmytro Turskyi <dmytro@turskyi.com>
2024-03-01 18:41:24 -0500

    add timeout 40 seconds to remote data source test
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- test/remote_data_source_impl_test.dart
+++ test/remote_data_source_impl_test.dart
@@ -20,36 +20,37 @@ void main() {
       ];
 
       OpenFoodAPIConfiguration.globalCountry = OpenFoodFactsCountry.CANADA;
+      final Dio dio = Dio();
+      const LoggingInterceptor loggingInterceptor = LoggingInterceptorImpl();
+      if (loggingInterceptor is Interceptor) {
+        dio.interceptors.add(loggingInterceptor as Interceptor);
+      }
 
       // Initialize the RemoteDataSourceImpl
-      remoteDataSource = RemoteDataSourceImpl(restClient);
+      remoteDataSource = RemoteDataSourceImpl(RetrofitClient(dio));
     });
 
-    test('getProductInfoAsFuture returns ProductInfo on success', () async {
-      // Arrange
-      const String input = '0055577105436';
-      const ProductInfo expectedResult = ProductInfo(
-        barcode: input,
-        country: 'Canada',
-      );
+    test(
+      'getProductInfoAsFuture returns ProductInfo on success',
+      () async {
+        // Arrange
+        const String input = '0055577105436';
+        const ProductInfo expectedResult = ProductInfo(
+          barcode: input,
+          country: 'Canada',
+        );
 
-      // Act
-      final ProductInfo result = await remoteDataSource.getProductInfoAsFuture(
-        const Barcode(code: input),
-      );
+        // Act
+        final ProductInfo result =
+            await remoteDataSource.getProductInfoAsFuture(
+          const Barcode(code: input),
+        );
 
-      // Assert
-      expect(result.country, equals(expectedResult.country));
-      expect(result.barcode, equals(expectedResult.barcode));
-    });
+        // Assert
+        expect(result.country, equals(expectedResult.country));
+        expect(result.barcode, equals(expectedResult.barcode));
+      },
+      timeout: const Timeout(Duration(seconds: 40)),
+    );
   });
-}
-
-RestClient get restClient {
-  final Dio dio = Dio();
-  const LoggingInterceptor loggingInterceptor = LoggingInterceptorImpl();
-  if (loggingInterceptor is Interceptor) {
-    dio.interceptors.add(loggingInterceptor as Interceptor);
-  }
-  return RetrofitClient(dio);
 }
