d771ffe2156221de8cdd13de7654d6106c7800ba
Dmytro Turskyi <dmytro@turskyi.com>
2025-06-23 20:14:49 -0400

    :memo: Improve feedback email formatting in `HomePresenter`
    - Added extra newlines in the generated feedback email body for better readability.
    - Ensured consistent newline formatting for app ID, version, build number, and platform information.
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- components/interface_adapters/lib/src/ui/modules/home/home_presenter.dart
+++ components/interface_adapters/lib/src/ui/modules/home/home_presenter.dart
@@ -455,16 +455,16 @@ class HomePresenter extends Bloc<HomeEvent, HomeViewModel> {
       final StringBuffer feedbackBody = StringBuffer()
         ..writeln('${isFeedbackType ? translate('feedback_type') : ''}:'
             ' ${isFeedbackType ? type.value : ''}')
-        ..writeln()
-        ..writeln(feedback.text)..writeln()..writeln(
+        ..writeln()..writeln(feedback.text)..writeln()..writeln(
             '${isFeedbackRating ? translate('rating') : ''}'
             '${isFeedbackRating ? ':' : ''}'
-            ' ${isFeedbackRating ? rating.value : ''}')..writeln()..writeln(
-            '${translate('app_id')}: ${packageInfo.packageName}')..writeln(
-            '${translate('app_version')}: ${packageInfo.version}')..writeln(
-            '${translate('build_number')}: ${packageInfo
-                .buildNumber}')..writeln()..writeln(
-            '${translate('platform')}: $platform')
+            ' ${isFeedbackRating ? rating.value : ''}')
+        ..writeln()
+        ..writeln('${translate('app_id')}: ${packageInfo.packageName}')
+        ..writeln('${translate('app_version')}: ${packageInfo.version}')
+        ..writeln('${translate('build_number')}: ${packageInfo.buildNumber}')
+        ..writeln()
+        ..writeln('${translate('platform')}: $platform')
         ..writeln();
       if (kIsWeb || Platform.isMacOS) {
         final Uri emailLaunchUri = Uri(
